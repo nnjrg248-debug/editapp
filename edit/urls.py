57a6b22c510475eac52807d49a693bb5548b4488
nakanishi <nnjrg248@gmail.com>
2026-04-13 11:12:47 +0900

    遷移ボタンテスト

--- edit/urls.py
+++ edit/urls.py
@@ -3,6 +3,8 @@ from django.urls import path,include
 from .import views
 from .views import PostListView,PostCreateView,PostUpdateView#作ったViweをインポート
 from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView,RedirectView
+from django.http import HttpResponse
 
 '''
 
@@ -34,5 +36,20 @@ urlpatterns=[
     path('delete/<int:pk>',views.memo_delete,name='memo_delete'),
   # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('ai-generate/',views.ai_generate,name='ai_generate'),
+
+    #viewなしで遷移できるTemplatesview 
+    path('aboutssss/',TemplateView.as_view(template_name="kkkk.html"),name="lllll"),
+    #view,templatesなしで遷移できるTemplatesview 「中身のない静的なページ」であれば、urls.pyに1行足すだけで、views.pyに何も書かずに遷移を実現
+    path('aboutssss2/',lambda request:HttpResponse("表示だけのテスト view、templatesなしでurlsに1行追加するだけでよいHttpResponse関数<br>またこのように改行もボタンも書ける<a href='/accounts/login/'>遷移ボタン</a>"),name="cccc") ,
+    #↑HttpResponese関数の文字列に追加した遷移ボタンの遷移先だが{% url %}の書き方はテンプレートの中だけで動く書き方、この場合はurlの指定を'/accounts/login/'とする、
+    #これでよい理由は親urlの上のほうでpath('accounts/', include('django.contrib.auth.urls'))としてるので
+    #親のaccountsとsetteingsのLOGIN_URL = 'login'（#未ログイン時飛ばされるところ、またviewにはname=’login’という指定はない）を組み合わせ'/accounts/login/'で指定している(このurl/accounts/login/'は頭に/をつけてるが、つけないと相対パス(その時のurlからのパス)になるので注意、
+    #またurlを/registration/login.htmlと直接指定しも遷移できないのは、ブラウザからのリクエストは必ず urls.py が受け取り、View（プログラム） が許可したものだけが画面に表示するので（Djangoはこの「直接指定」を禁止）(通常http://.../login.htmlとすれば開くかもしれんが)
+    #またpath('aboutssss2/',lambda,HTTp.Response( ～としているが
+    #↑もう1つ上のpath関数の第一引数aboutssssと被ると行が無視されるとのこと名でaboutssss2とした
+
+    # 上記path関数の場合文字列を表示させるHPに移動させてボタン押させloginページに飛ばすが直接ログインp-時に飛ばすときは以下の RedirectView.asview(url'/accounts/login/'～とする
+    path('old-link/', RedirectView.as_view(url='/accounts/login/'), name='go-to-login'),
+   
     
 ]
